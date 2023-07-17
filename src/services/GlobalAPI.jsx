3b586675a81fc6cc66f451cc32f34154437ecfc5
Bima Setyo <abimanyusrisetyo@gmail.com>
2023-07-17 18:03:43 +0700

    Fix GlobalAPI.jsx

--- src/services/GlobalAPI.jsx
+++ src/services/GlobalAPI.jsx
@@ -5,7 +5,7 @@ const api_key = "4a0a1d61ff1ee83055a4d673d9a3e5e9";
 
 // Trending Videos
 const getTrendingVideos = axios.get(
-  this.movieBaseUrl + "/trending/all/day?api_key" + api_key
+  movieBaseUrl + "/trending/all/day?api_key=" + api_key
 );
 export default {
   getTrendingVideos,
