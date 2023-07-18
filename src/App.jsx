bbe6dffb25b96a8c5d27248fdd416780f9218ede
Bima Setyo <abimanyusrisetyo@gmail.com>
2023-07-18 11:27:31 +0700

    Add ProductionHouse.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Slider from "./components/Slider";
+import ProductionHouse from "./components/ProductionHouse";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +13,7 @@ function App() {
     <>
       <Header />
       <Slider />
+      <ProductionHouse />
     </>
   );
 }
---
+++ src/components/ProductionHouse.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProductionHouse() {
+  return <div>ProductionHouse</div>;
+}
+
+export default ProductionHouse;
