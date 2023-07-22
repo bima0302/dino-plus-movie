05cad328a3b3bc7f217a0adbed34dc9678b7e9ae
Bima Setyo <abimanyusrisetyo@gmail.com>
2023-07-22 13:56:56 +0700

    Fix autoplay on hover

--- src/components/ProductionHouse.jsx
+++ src/components/ProductionHouse.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-// Images
 import disney from "./../assets/Images/disney.png";
 import marvel from "./../assets/Images/marvel.png";
 import nationalG from "./../assets/Images/nationalG.png";
 import pixar from "./../assets/Images/pixar.png";
 import starwar from "./../assets/Images/starwar.png";
-// Videos
+
+import starwarV from "./../assets/Videos/star-wars.mp4";
 import disneyV from "./../assets/Videos/disney.mp4";
 import marvelV from "./../assets/Videos/marvel.mp4";
-import nationalGV from "./../assets/Videos/national-geographic.mp4";
+import nationalGeographicV from "./../assets/Videos/national-geographic.mp4";
 import pixarV from "./../assets/Videos/pixar.mp4";
-import starwarV from "./../assets/Videos/star-wars.mp4";
 
 function ProductionHouse() {
-  const ProductionHouseList = [
+  const productionHouseList = [
     {
       id: 1,
       image: disney,
@@ -21,37 +20,44 @@ function ProductionHouse() {
     },
     {
       id: 2,
-      image: marvel,
-      video: marvelV,
-    },
-    {
-      id: 3,
-      image: nationalG,
-      video: nationalGV,
-    },
-    {
-      id: 4,
       image: pixar,
       video: pixarV,
     },
     {
-      id: 5,
+      id: 3,
+      image: marvel,
+      video: marvelV,
+    },
+    {
+      id: 4,
       image: starwar,
       video: starwarV,
     },
+    {
+      id: 5,
+      image: nationalG,
+      video: nationalGeographicV,
+    },
   ];
   return (
-    <div className="flex gap-2 md:gap-5 p-2 px-5 md:px-16">
-      {ProductionHouseList.map((item) => (
-        <div className="border-[2px] border-gray-600 rounded-lg hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer relative shadow-xl shadow-black">
-          <img src={item.image} className="w-full z-1" />
+    <div className="flex gap-2 md:gap-5 p-2 px-5 md:px-16 ">
+      {productionHouseList.map((item) => (
+        <div
+          className="border-[2px] border-gray-600
+            rounded-lg hover:scale-110 transition-all duration-300
+            ease-in-out cursor-pointer relative shadow-xl 
+            shadow-gray-800"
+        >
           <video
             src={item.video}
             autoPlay
             loop
             playsInline
-            className="absolute top-0 rounded-md z-0 opacity-0 hover:opacity-50"
-          ></video>
+            muted
+            className="absolute z-0  top-0 rounded-md 
+            opacity-0 hover:opacity-50"
+          />
+          <img src={item.image} className="w-full z-[1] opacity-100" />
         </div>
       ))}
     </div>
